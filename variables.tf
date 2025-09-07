b263886ebabcf9239dc7e02adb67c6487d78334f
outofmonkey <128472571+outofmonkey@users.noreply.github.com>
2025-09-07 23:36:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,8 +2,3 @@ variable "apis_to_enable" {
   type        = list(string)
   description = "List of APIs to enable"
 }
-
-variable "project_id" {
-  type        = string
-  description = "Project ID"
-}
